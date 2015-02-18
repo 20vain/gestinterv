289def983e1618046f9b52c1cc203566c1ae5d3b
Julien HOMMET <julien@computerz.solutions>
2015-02-18 21:16:55 +0100

    Ajout jquery pour affichage "dynamique"

--- admin/index.php
+++ admin/index.php
@@ -1,26 +1,59 @@
-﻿<center><h2>Gestion du système GESTINTERV</h2></center>
+﻿<head>
+	<script> <!-- Affichage du panneau selon le bouton cliqué -->
+	$(document).ready(function(){
+		$("#flip1").click(function(){
+			$("#panel1").slideToggle("slow");
+		});
+	});
+	
+	$(document).ready(function(){
+		$("#flip2").click(function(){
+			$("#panel2").slideToggle("slow");
+		});
+	});
+	
+	$(document).ready(function(){
+		$("#flip3").click(function(){
+			$("#panel3").slideToggle("slow");
+		});
+	});
+	
+	$(document).ready(function(){
+		$("#flip4").click(function(){
+			$("#panel4").slideToggle("slow");
+		});
+	});
+	</script>
+
+</head>
+
+<center><h2>Gestion du système GESTINTERV</h2></center>
 
 <div class="container">
 
-<table class="table">
-	<tr>
-		<td>
-			<?php include ("typemateriel.php"); ?>
-		</td>
-		
-		<td>
-			<?php include ("typeinterv.php"); ?>
-		</td>
-		
-		<td>
-			<?php include ("techniciens.php"); ?>
-		</td>
-		
-		<td>
-			<?php include ("logiciels.php"); ?>
-		</td>
-	</tr>
-</table>
+<div id="flip1"><button class="btn btn-large">Liste des MATERIELS</button></div><br />
+<div id="flip2"><button class="btn btn-large">Liste des TYPES D'INTERVENTION</button></div><br />
+<div id="flip3"><button class="btn btn-large">Liste des TECHNICIENS</button></div><br />
+<div id="flip4"><button class="btn btn-large">Liste des LOGICIELS</button></div>
+
+
+<div id="panel1">
+	<?php include ("typemateriel.php"); ?>
+</div>
+
+<div id="panel2">
+	<?php include ("typeinterv.php"); ?>
+</div>
+
+<div id="panel3">
+	<?php include ("techniciens.php"); ?>
+</div>
+
+<div id="panel4">
+	<?php include ("logiciels.php"); ?>
+</div>
+
+
 <hr />
 <?php include ("stats.php"); ?>
 
